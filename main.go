dfe21228e7fb6b43d46a29ce154fc80d1904a6b9
G4mer1808 <44718065+G4mer1808@users.noreply.github.com>
2020-08-31 21:16:19 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/faiface/beep"
@@ -16,8 +12,9 @@ import (
 )
 
 //Some globally used variables
-var bearerToken string = "x3oyoq4z6291bw6iuqhnnwyzusnasq"
-var clientID string = "5qzrrwjwsuum85molwsalbv1q5v1f5"
+var bearerToken string //loaded via loadConfig()
+var clientID string    //loaded via loadConfig()
+var soundFile string   //loaded via loadConfig()
 var streamInfo stream
 
 //Definitions of all the flags
@@ -26,7 +23,7 @@ var retryInterval int
 
 func main() {
 	parseFlags()
-
+	loadConfig()
 	startTime := time.Now()
 	elapsed := int(time.Now().Sub(startTime))
 	_ = elapsed //Bypass unused error
@@ -69,17 +66,14 @@ func main() {
 	}
 
 	fmt.Printf("Category changed to: " + newGameName)
+	playSound()
+}
 
-	audioFile, err := os.Open("juntos.ogg")
-	if err != nil {
-		fmt.Printf("Error opening audio file juntos.ogg\n")
-		os.Exit(1)
-	}
+func playSound() {
+	audioFile, err := os.Open(soundFile)
+	checkError(err)
 	audioStreamer, audioFormat, err := vorbis.Decode(audioFile)
-	if err != nil {
-		fmt.Printf("Error decoding audio file juntos.ogg\n")
-		os.Exit(1)
-	}
+	checkError(err)
 	defer audioStreamer.Close()
 	speaker.Init(audioFormat.SampleRate, audioFormat.SampleRate.N(time.Second/10))
 	done := make(chan bool)
@@ -87,18 +81,20 @@ func main() {
 		done <- true
 	})))
 	<-done
+	audioFile.Close()
 }
 
 func getStreamInfoWithOnlineCheck() {
 	streamInfo = getStreamInfo(streamName)
 
 	if streamInfo.Data.StartedAt == "" {
-		fmt.Printf("Channel offline. Waiting for change (Checking every " + fmt.Sprint(retryInterval) + "s)\n")
+		fmt.Printf(streamName + " is currently offline. Waiting for change (Checking every " + fmt.Sprint(retryInterval) + "s)\n")
 		for streamInfo.Data.StartedAt == "" {
 			waitRetryInterval()
 			streamInfo = getStreamInfo(streamName)
 		}
-		fmt.Printf("Channel went online\n")
+		fmt.Printf(streamName + " just went online\n")
+		playSound()
 	}
 }
 
@@ -115,69 +111,4 @@ func parseFlags() {
 	flag.StringVar(&streamName, "c", "xqcow", "provide the name of the twitch channel")
 	flag.IntVar(&retryInterval, "t", 10, "provide the interval (in seconds) in which to refresh the stream's information")
 	flag.Parse()
-}
-
-func getGameInfo(gameID string) game {
-	httpClient := &http.Client{}
-	req, _ := http.NewRequest("GET", "https://api.twitch.tv/helix/games?id="+gameID, nil)
-	req.Header.Add("Authorization", "Bearer "+bearerToken)
-	req.Header.Add("Client-ID", clientID)
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		fmt.Printf("Error retrieving game info\n")
-		os.Exit(1)
-	}
-	body, _ := ioutil.ReadAll(resp.Body)
-	gameJSON := string(body)
-	gameJSON = strings.ReplaceAll(gameJSON, "[", "")
-	gameJSON = strings.ReplaceAll(gameJSON, "]", "")
-	var gameInfo game
-	json.Unmarshal([]byte(gameJSON), &gameInfo)
-	return gameInfo
-}
-
-type gameData struct {
-	BoxArtURL string `json:"box_art_url"`
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-}
-
-type game struct {
-	Data gameData
-}
-
-func getStreamInfo(streamerName string) stream {
-	httpClient := &http.Client{}
-	req, _ := http.NewRequest("GET", "https://api.twitch.tv/helix/streams?user_login="+streamerName, nil)
-	req.Header.Add("Authorization", "Bearer "+bearerToken)
-	req.Header.Add("Client-ID", clientID)
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		fmt.Printf("Error retrieving stream info\n")
-		os.Exit(1)
-	}
-	body, _ := ioutil.ReadAll(resp.Body)
-	streamJSON := string(body)
-	streamJSON = strings.ReplaceAll(streamJSON, "[", "")
-	streamJSON = strings.ReplaceAll(streamJSON, "]", "")
-	json.Unmarshal([]byte(streamJSON), &streamInfo)
-	return streamInfo
-}
-
-type streamData struct {
-	ID           string `json:"id"`
-	UserID       string `json:"user_id"`
-	UserName     string `json:"user_name"`
-	GameID       string `json:"game_id"`
-	Type         string `json:"type"`
-	Title        string `json:"title"`
-	ViewerCount  string `json:"viewer_count"`
-	StartedAt    string `json:"started_at"`
-	Language     string `json:"language"`
-	ThumbnailURL string `json:"thumbnail_url"`
-	TagIDs       string `json:"tag_ids"`
-}
-
-type stream struct {
-	Data streamData
 }
